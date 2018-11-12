d0f5a0e290656dbc6727d4028b0a7ed4a4354fcb
Adm <tarasklyushta@gmail.com>
2018-11-12 23:27:49 +0200

    Add storybook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Button from './components/Button';
 import './App.css';
 
 class App extends Component {
 	render () {
 		return (
-			<h1>Hello world</h1>
+			<Button>Click me</Button>
 		);
 	}
 }
