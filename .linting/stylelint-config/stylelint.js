cc65f194a24331b667e8f3f856d4434569d6c58b
Taras Kliushta <tarasklyushta@gmail.com>
2018-11-12 22:37:32 +0200

    Setup initial scaffolding (#1)

---
+++ .linting/stylelint-config/stylelint.js
@@ -0,0 +1,25 @@
+/* eslint-disable quote-props */
+
+module.exports = {
+	'extends': 'stylelint-config-standard',
+	'rules': {
+		'color-no-hex': true,
+		'font-family-name-quotes': 'always-where-recommended',
+		'font-weight-notation': 'numeric',
+		'indentation': 'tab',
+		'max-nesting-depth': 2,
+		'no-duplicate-selectors': true,
+		'number-leading-zero': 'never',
+		'property-no-unknown': [
+			true,
+			{
+				'ignoreProperties': [
+					'composes',
+				],
+			},
+		],
+		'selector-max-id': 0,
+		'string-quotes': 'single',
+		'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['global'] }],
+	},
+};
