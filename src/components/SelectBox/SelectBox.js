5e89eb7d51ff158e4535cb5252732ad6715c6727
ktv18 <tarasklyushta@gmail.com>
2018-11-13 19:07:04 +0200

    Add SelectBox component

---
+++ src/components/SelectBox/SelectBox.js
@@ -0,0 +1,120 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import Input from '@material-ui/core/Input';
+import MenuItem from '@material-ui/core/MenuItem';
+
+import { createMuiTheme, MuiThemeProvider, withStyles } from '@material-ui/core/styles';
+
+const stylesObj = (theme) => ({
+	formControl: {
+		margin: theme.spacing.unit,
+		width: '90%',
+	},
+	selectEmpty: {
+		marginTop: theme.spacing.unit * 2,
+	},
+});
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: '#2bd1b3',
+			contrastText: '#fff',
+		},
+	},
+});
+
+class SelectBox extends Component {
+
+	static propTypes = {
+		classes: PropTypes.object,
+		disabled: PropTypes.array.bool,
+		error: PropTypes.array.bool,
+		label: PropTypes.string.isRequired,
+		name: PropTypes.string.isRequired,
+		noneLabel: PropTypes.string,
+		onChange: PropTypes.func.isRequired,
+		options: PropTypes.array.isRequired,
+		outlined: PropTypes.array.bool,
+	}
+
+	state = {
+		[this.props.name]: '',
+		labelWidth: 0,
+	}
+
+	inputLabelRef = React.createRef();
+
+	handleChange = ({ target }) => {
+		const { name, value } = target;
+		this.setState({
+			[name]: value,
+		}, this.props.onChange(value));
+	}
+
+	setLabelWidth (labelWidth) {
+		this.setState({ labelWidth });
+	}
+
+	getInputId ({ name } = this.props) {
+		return `input-${name}-id`;
+	}
+
+	componentDidMount () {
+		if (this.props.outlined) {
+			const refDomNode = ReactDOM.findDOMNode(this.inputLabelRef.current);
+			this.setLabelWidth(refDomNode.offsetWidth);
+		}
+	}
+
+	getSelectInput ({ outlined, name } = this.props) {
+		if (!outlined) return <Input name={name} id={this.getInputId()} />;
+		return (
+			<OutlinedInput
+				labelWidth={this.state.labelWidth}
+				name={name}
+				id={this.getInputId()}
+			/>
+		);
+	}
+
+	render () {
+		const { classes, options = [], name, label, noneLabel, error, disabled, outlined } = this.props;
+		return (
+			<MuiThemeProvider theme={theme}>
+				<FormControl
+					variant={outlined ? 'outlined' : ''}
+					className={classes.formControl}
+					error={error}
+					disabled={disabled}
+				>
+					<InputLabel
+						ref={this.inputLabelRef}
+						htmlFor={this.getInputId()}>
+						{label}
+					</InputLabel>
+					<Select
+						value={this.state[name]}
+						onChange={this.handleChange}
+						input={this.getSelectInput()}
+					>
+						{noneLabel && (
+							<MenuItem value=''>
+								<em>{noneLabel}</em>
+							</MenuItem>
+						)}
+						{options
+							.map(({ value, name }) => <MenuItem	key={value} value={value}>{name}</MenuItem>)}
+					</Select>
+				</FormControl>
+			</MuiThemeProvider>
+		);
+	}
+}
+
+export default withStyles(stylesObj)(SelectBox);
---
+++ src/components/SelectBox/story.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { text, boolean } from '@storybook/addon-knobs/react';
+
+import SelectBox from './SelectBox';
+
+const options = [
+	{
+		value: '_id_1',
+		name: 'guitar',
+	},
+	{
+		value: '_id_2',
+		name: 'drum',
+	},
+	{
+		value: '_id_3',
+		name: 'saxophone',
+	},
+	{
+		value: '_id_4',
+		name: 'violin',
+	},
+];
+
+const name = 'type';
+const defaultlabel = 'some default label';
+const defaultNonelabel = 'none';
+
+const props = () => ({
+	options,
+	name,
+	outlined: boolean('outlined', true),
+	disabled: boolean('disabled', false),
+	error: boolean('error', false),
+	label: text('label', defaultlabel),
+	noneLabel: text('none', defaultNonelabel),
+	onChange: () => {},
+});
+
+storiesOf('SelectBox', module)
+	.add('with text', () => <SelectBox {...props()} />);
